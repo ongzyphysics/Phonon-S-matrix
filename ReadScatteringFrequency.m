a631cb27a8cf39198d7fa3a509345cb03a2b8e94
ongzyphysics <38254726+ongzyphysics@users.noreply.github.com>
2018-04-16 13:09:17 +0100

    Add files via upload

--- ReadScatteringFrequency.m
+++ ReadScatteringFrequency.m
@@ -1,21 +1,22 @@
 function wvec_scatter = ReadScatteringFrequency(DataFilesDir)
+% NOTE: Reads the frequency points for outputting channel scattering data
+% NOTE: This is optional. If file is not found, then we assume no channel scattering data output is wanted
 
-CurrDir = pwd;
+  CurrDir = pwd;
 
-cd(DataFilesDir);
+  cd(DataFilesDir);
 
-filename = 'Scattering_Output_Frequency.agf';
-fid = fopen(filename,'r');
+  filename = 'Scattering_Output_Frequency.agf'; % data file name 
+  fid = fopen(filename,'r');
 
-if gt(fid,0)
-    data = importdata(filename,' ',0); % read file containing range of frequency values  
-    wvec_scatter = data(:,1);
-else
-    warning(sprintf('<!!!> %s not found.',filename));
-    wvec_scatter = 0;
+  if gt(fid,0)
+      data = importdata(filename,' ',0); % read file containing range of frequency values  
+      wvec_scatter = data(:,1);
+  else
+      warning(sprintf('<!!!> %s not found.',filename));
+      wvec_scatter = 0;
+  end
+  fclose(fid);
+
+  cd(CurrDir);
 end
-fclose(fid);
-
-
-
-cd(CurrDir);
